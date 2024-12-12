2c8f7794b9ac202787f675a93bf67a84195740a4
S-HariPrasath <86016964+S-HariPrasath@users.noreply.github.com>
2024-12-12 11:42:49 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/data/headerData.js
@@ -0,0 +1,9 @@
+import resume from '../assets/pdf/resume.pdf'
+
+export const headerData = {
+    name: 'HariPrasath S',
+    title: "Application Engineer",
+    desciption:"I am known for excellence in defining and integrating cutting-edge technologies to improve automation and reliability.",
+    image: '',
+    resumePdf: resume
+}
